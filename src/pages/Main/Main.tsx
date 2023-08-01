99613e26d009e9c296d366dec5af56b2f3f5324b
EaleksS <ealgenv@gmail.com>
2023-08-01 22:48:18 +0900

    animation

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -7,6 +7,40 @@ export const Main: FC = (): JSX.Element => {
       <div style={{ display: "flex", alignItems: "center", height: "80vh" }}>
         <Slider />
       </div>
+      <div className="background">
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+        <div className="cube"></div>
+      </div>
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
     </Layout>
   );
 };
