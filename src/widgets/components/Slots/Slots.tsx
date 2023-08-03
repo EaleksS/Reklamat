d411867556f81814bcdbcc38fd51b436e4593247
EaleksS <ealgenv@gmail.com>
2023-08-03 23:01:07 +0900

    .

--- src/widgets/components/Slots/Slots.tsx
+++ src/widgets/components/Slots/Slots.tsx
@@ -1,131 +1,129 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import styles from "./Slots.module.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-
-import { Autoplay } from "swiper/modules";
+import SlotCounter, { SlotCounterRef } from "react-slot-counter";
+import { useStore } from "../../../store/store";
 
 export const Slots: FC = (): JSX.Element => {
+  const counterRef1 = useRef<SlotCounterRef>(null);
+  const counterRef2 = useRef<SlotCounterRef>(null);
+  const counterRef3 = useRef<SlotCounterRef>(null);
+  const counterRef4 = useRef<SlotCounterRef>(null);
+
+  const { begin, setBegin } = useStore();
+
+  useEffect(() => {
+    if (begin) {
+      counterRef1.current?.startAnimation();
+      counterRef2.current?.startAnimation();
+      counterRef3.current?.startAnimation();
+      counterRef4.current?.startAnimation();
+
+      setBegin(false);
+    }
+  }, [begin]);
+
   return (
     <div className={styles.slots}>
-      <Swiper
-        direction={"vertical"}
-        slidesPerView={3}
-        spaceBetween={50}
-        centeredSlides={true}
-        className={styles.mySwiper}
-        modules={[Autoplay]}
-        loop
-        loopedSlides={0}
-        autoplay={{ delay: 100 }}
-      >
-        <SwiperSlide>
-          <img src="/slide1.png" alt="slider" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/slide1.png" alt="slider" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/slide1.png" alt="slider" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/slide1.png" alt="slider" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/slide1.png" alt="slider" />
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        direction={"vertical"}
-        slidesPerView={3}
-        spaceBetween={50}
-        centeredSlides={true}
-        className={styles.mySwiper}
-        modules={[Autoplay]}
-        loop
-        loopedSlides={0}
-        autoplay={{ delay: 100 }}
-      >
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        direction={"vertical"}
-        slidesPerView={3}
-        spaceBetween={50}
-        centeredSlides={true}
-        className={styles.mySwiper}
-        modules={[Autoplay]}
-        loop
-        loopedSlides={0}
-        autoplay={{ delay: 100 }}
-      >
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        direction={"vertical"}
-        slidesPerView={3}
-        spaceBetween={50}
-        centeredSlides={true}
-        className={`${styles.mySwiper} ${styles.noborder}`}
-        modules={[Autoplay]}
-        loop
-        loopedSlides={0}
-        autoplay={{ delay: 100 }}
-      >
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/Frame.png" alt="slider" className={styles.frame} />
-        </SwiperSlide>
-      </Swiper>
+      <SlotCounter
+        ref={counterRef1}
+        containerClassName={styles.cont}
+        charClassName={styles.char}
+        valueClassName={styles.value}
+        separatorClassName={styles.separ}
+        duration={2}
+        // startValueOnce
+        // sequentialAnimationMode
+        animateUnchanged
+        startValue={[
+          <img
+            className={styles.item}
+            style={{ transform: "translateY(-70px)" }}
+            src="/slide1.png"
+            alt=""
+          />,
+        ]}
+        value={[<img className={styles.item} src="/slide1.png" alt="" />]}
+        dummyCharacters={[
+          <img className={styles.item} src="/slide1.png" alt="" />,
+          <img className={styles.item} src="/slide1.png" alt="" />,
+          <img className={styles.item} src="/slide1.png" alt="" />,
+          <img className={styles.item} src="/slide1.png" alt="" />,
+          <img className={styles.item} src="/slide1.png" alt="" />,
+        ]}
+      />
+      <SlotCounter
+        ref={counterRef2}
+        containerClassName={styles.cont}
+        charClassName={styles.char}
+        valueClassName={styles.value}
+        separatorClassName={styles.separ}
+        duration={2}
+        startValue={[
+          <img
+            className={styles.item}
+            style={{ transform: "translateY(-70px)" }}
+            src="/Frame.png"
+            alt=""
+          />,
+        ]}
+        value={[<img className={styles.item} src="/Frame.png" alt="" />]}
+        dummyCharacters={[
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+        ]}
+      />
+      <SlotCounter
+        ref={counterRef3}
+        containerClassName={styles.cont}
+        charClassName={styles.char}
+        valueClassName={styles.value}
+        separatorClassName={styles.separ}
+        duration={2}
+        // sequentialAnimationMode
+        startValue={[
+          <img
+            className={styles.item}
+            style={{ transform: "translateY(-70px)" }}
+            src="/Frame.png"
+            alt=""
+          />,
+        ]}
+        value={[<img className={styles.item} src="/Frame.png" alt="" />]}
+        dummyCharacters={[
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+        ]}
+      />
+      <SlotCounter
+        ref={counterRef4}
+        containerClassName={styles.cont}
+        charClassName={styles.char}
+        valueClassName={styles.value}
+        separatorClassName={styles.separ}
+        duration={2}
+        startValue={[
+          <img
+            className={styles.item}
+            style={{ transform: "translateY(-70px)" }}
+            src="/Frame.png"
+            alt=""
+          />,
+        ]}
+        value={[<img className={styles.item} src="/Frame.png" alt="" />]}
+        dummyCharacters={[
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+          <img className={styles.item} src="/Frame.png" alt="" />,
+        ]}
+      />
     </div>
   );
 };
