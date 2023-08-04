89b16fe7c0e4a1f0fda90dd13ab0252420f794fe
EaleksS <ealgenv@gmail.com>
2023-08-04 19:41:52 +0900

    .

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,18 @@ import "./styles/_variables.scss";
 import { Routing } from "./providers/Routing";
 import { routes } from "../pages";
 import "./styles/reset.min.css";
+import { PSuspense } from "./providers/Suspense";
 
 export const App = () => {
   return (
-    <Routing>
-      <Routes>
-        {routes.map((e) => (
-          <Route {...e} />
-        ))}
-      </Routes>
-    </Routing>
+    <PSuspense>
+      <Routing>
+        <Routes>
+          {routes.map((e) => (
+            <Route {...e} />
+          ))}
+        </Routes>
+      </Routing>
+    </PSuspense>
   );
 };
